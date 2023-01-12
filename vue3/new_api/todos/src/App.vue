28a614b5cf607501be78cf9212a425d44717386d
jingg12 <2601127998@qq.com>
2023-01-12 13:43:39 +0800

    111

---
+++ vue3/new_api/todos/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 @click="add" class="red">{{count}}---</h1>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+// ref 响应式 简单值
+
+let count = ref(1);
+function add() {
+  count++;
+  
+}
+</script>
+<style>
+.red {
+  color: red;
+}
+</style>
