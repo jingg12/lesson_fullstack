804be5d510dce75b9bb0bf99c1620b924cb61d6d
jingg12 <2601127998@qq.com>
2023-02-06 15:54:15 +0800

    111

--- js/clone/1.js
+++ js/clone/1.js
@@ -1,2 +1,13 @@
 // const clone = require ('./utils');
-import clone from './util';//es6
+import clone from './util.js';//es6
+const target = {
+    field1: 1,
+    field2: undefined,
+    field3: {
+        child: 'child'
+    },
+    field4: [2, 4, 8]
+};
+target.target = target;// 循环引用 栈溢出  递归没有了退出条件
+
+// JSON.parse(JSON.stringify(target))// 不行
--- js/clone/readme.md
+++ js/clone/readme.md
@@ -2,4 +2,4 @@
   es5 没有模块化机制的， 大型语言， 大型项目必备 js 解释器和编辑器 chrome v8 引擎 dom css渲
   染
   node 出现的时候 js运行时可以在后端环境中执行 后端项目  稳定性， 大型 commonjs
-  es6 js 全新的模块化 vue react import export
+  es6 js  升级了全新的模块化 vue react import export
